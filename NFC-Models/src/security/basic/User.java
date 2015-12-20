f8d9bd797f3436b20dbdc165f4b13c8077ad9e81
willsmith9182 <willsmith182@gmail.com>
2015-12-20 21:23:11 +0000

    basic user declaration

---
+++ NFC-Models/src/security/basic/User.java
@@ -0,0 +1,44 @@
+package security.basic;
+
+import java.util.ArrayList;
+
+import security.Key;
+import security.Role;
+
+public class User {
+
+	private final String _username;
+	private final String _password;
+	private final Integer _userId;
+	private final Key _key;
+	private final ArrayList<Role> _roles;
+
+	public User(int userId, String username, String password, Key key, ArrayList<Role> roles) {
+		_userId = userId;
+		_password = password;
+		_username = username;
+		_key = key;
+		_roles = roles;
+	}
+
+	public ArrayList<Role> get_Roles() {
+		return _roles;
+	}
+
+	public String get_username() {
+		return _username;
+	}
+
+	public String get_password() {
+		return _password;
+	}
+
+	public Integer get_userId() {
+		return _userId;
+	}
+
+	public Key get_key() {
+		return _key;
+	}
+
+}
